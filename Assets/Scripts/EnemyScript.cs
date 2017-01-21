0b23f4cf2089e80c0ebd58e36cb2acaba6e56b9b
Clonch <tc553413@dhcp-237-148.cns.ohiou.edu>
2017-01-21 18:39:43 -0500

    Untested art assets added and enemies spawn without randomization.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -10,11 +10,17 @@ public class EnemyScript : MonoBehaviour
 	float fractJourney = 0.01f; //Starts the player 1% of the way toward the player so that it isn't destroyed on spawn.
 	float speed; //Speed of the player in percent of distanced traveled per second. e.g. 0.50 is the enemy will move 50% of the way to the player each second.
 
+
+
+
 	void Start ()
 	{
 		startPos = transform.position;
 		speed = startSpeed;
 	}
+
+
+
 	void Update () 
 	{
 		if (Input.GetButton(""+button) ) //Checks if the assigned button is pressed and if so moves away from the player, if not, moves toward him.
@@ -25,14 +31,17 @@ public class EnemyScript : MonoBehaviour
 			speed = startSpeed;
 		}
 
+
 		fractJourney += speed * Time.deltaTime;
 		transform.position = Vector3.Lerp(startPos, player.transform.position, fractJourney); //Moves the enemy fractJourney percent of the way toward/away from the player.
+
 
 		if (Vector2.Distance (new Vector2 (transform.position.x, transform.position.z), new Vector2 (player.transform.position.x, player.transform.position.z)) <= 1) //If the enemy is close enough to the player, run the damage function on the player.
 		{
 			player.SendMessage ("Damage");
 			Destroy (gameObject);
 		}
+
 
 		if (transform.position == startPos) 
 		{
--- Assets/Scripts/EnemyWaveScript.cs
+++ Assets/Scripts/EnemyWaveScript.cs
@@ -1,18 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyWaveScript : MonoBehaviour {
+public class EnemyWaveScript : MonoBehaviour 
+{
 
     public int waveNumber; //the wave number
     public int enemiesLeft; //number of enemies currently left in the wave
     public float spawnMin; //min time to wait until spawning next enemy
     public float spawnMax; //max time to wait until spawning the next enemy
-    struct EnemyData {
+	public GameObject enemyPrefab;
+	public GameObject player;
+	public Sprite[] anxiousSprites;
+	public Sprite[] characterSprites;
+	public char[] buttons;
+	GameObject lastEnemy;
+	Vector3 spawnPoint;
+	char button;
+	int buttonIndex;
+
+    struct EnemyData 
+	{
       int speed;
       string button;
     }
 
-    EnemyWaveScript(int waveNumber)
+
+
+	void Update()
+	{
+		if (enemiesLeft >= 1) 
+		{
+			spawnEnemy ();
+		}
+	}
+
+
+
+/*    EnemyWaveScript(int waveNumber)
     {
         //assign variables
         this.waveNumber = waveNumber;
@@ -22,15 +46,26 @@ public class EnemyWaveScript : MonoBehaviour {
         //TODO calculate other variables and define a default enemy
         //calc speed  y = sqrt{50x}+5
     }
+*/
+
 
     EnemyScript spawnEnemy()
     {
-        if (enemiesLeft > 1)
-        {
-            enemiesLeft--;
+		spawnPoint = new Vector3 (10,0,0);
 
-            //TODO spawn enemy
-        }
+
+		buttonIndex = Random.Range (0, buttons.Length);
+		button = buttons [buttonIndex];
+		for(int i = 0; i < characterSprites.Length; i++)
+		{
+			
+		}
+
+
+		lastEnemy = (GameObject)Instantiate (enemyPrefab, spawnPoint, Quaternion.Euler(new Vector3(90,0,0)));
+		lastEnemy.GetComponent<EnemyScript> ().player = player;
+		enemiesLeft--;
+		return lastEnemy.GetComponent<EnemyScript> ();
+		//TODO spawn enemy
     }
-
 }
