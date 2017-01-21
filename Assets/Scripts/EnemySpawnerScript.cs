5e642638abd68dc32af63a59c48994644ed74685
Jake Cirino <jakecirino@fangergames.com>
2017-01-21 16:00:06 -0500

    Enemy spawning works, need to edit properties of newly spawned objects

--- Assets/Scripts/EnemySpawnerScript.cs
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemySpawner : MonoBehaviour {
+public class EnemySpawnerScript : MonoBehaviour {
 
-    
+    public int maxObjects;
+    public Sprite[] enemySprites;
+    public GameObject[] ingameObjects;
+    float time = 0;
+    EnemyWaveScript waveScript;
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    void Start () {
+        waveScript = GetComponentInParent<EnemyWaveScript>();
+        waveScript.resetWave(10);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        time += Time.deltaTime;
+        print(time);
+        if(time > 2)
+        {
+            waveScript.spawnEnemy('k'); //spawn enemy
+            time = 0; //reset time
+        }
 	}
 }
--- Assets/Scripts/EnemyWaveScript.cs
+++ Assets/Scripts/EnemyWaveScript.cs
@@ -7,29 +7,24 @@ public class EnemyWaveScript : MonoBehaviour {
     public int enemiesLeft; //number of enemies currently left in the wave
     public float minSpawnTime; //min time to wait until spawning next enemy
     public float maxSpawnTime; //max time to wait until spawning the next enemy
-    private GameObject model; //the enemy to copy for each wave
-    
-    struct enemyInfo //contains all the info for each enemy
-    {
-        float speed;
-        Sprite sprite;
-    };
+    public GameObject model; //the enemy to copy for each wave
 
-    EnemyWaveScript(int waveNumber)
+    public void resetWave(int waveNumber) //changes the wave to this wave
     {
         //assign variables
         this.waveNumber = waveNumber;
 
         //calculate data based on wave
         enemiesLeft = (int)Mathf.Sqrt(40 * waveNumber) + Random.Range(2, 8) + 15; //y = .5x + 10
+        //TODO calculate max and min spawn time
 
-        model = (GameObject)Instantiate(Resources.Load("Enemy")); //create the default model enemy
         EnemyScript modelScript = (EnemyScript)model.GetComponent(typeof(EnemyScript)); //get the script of the enemy
         modelScript.speed = Mathf.Sqrt(50 * waveNumber) + 5; //y = sqrt{50x}+5
     }
 
-    GameObject spawnEnemy(char key)
+    public GameObject spawnEnemy(char key)
     {
+        print("Called");
         if (enemiesLeft > 1) { //if there are still enemies left in this wave
             enemiesLeft--;
 
