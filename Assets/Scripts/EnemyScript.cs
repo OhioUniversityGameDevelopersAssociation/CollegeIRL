3983f391411cc71b033ececb91f3df03d70f8e15
Clonch <tc553413@dhcp-237-148.cns.ohiou.edu>
2017-01-20 23:57:29 -0500

    I commented better  and cleaned up redundant code.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,12 +5,10 @@ public class EnemyScript : MonoBehaviour
 {
 	public GameObject player;
 	public Vector3 startPos;
-	public int health;
-	public bool buttonHeld;
 	public float startSpeed;
-	public char button;
-	float fractJourney = 0.01f;
-	float speed;
+	public char button; //Assigned button you must press to move the enemy away, assigned by the spawning script.
+	float fractJourney = 0.01f; //Starts the player 1% of the way toward the player so that it isn't destroyed on spawn.
+	float speed; //Speed of the player in percent of distanced traveled per second. e.g. 0.50 is the enemy will move 50% of the way to the player each second.
 
 	void Start ()
 	{
@@ -19,29 +17,19 @@ public class EnemyScript : MonoBehaviour
 	}
 	void Update () 
 	{
-		if (Input.GetButton(""+button) )
+		if (Input.GetButton(""+button) ) //Checks if the assigned button is pressed and if so moves away from the player, if not, moves toward him.
 		{
-			buttonHeld = true;
-			print ("Button pressed");
+			speed = -startSpeed;
 		} else
 		{
-			buttonHeld = false;
-		}
-
-		if(buttonHeld == false)
-		{
 			speed = startSpeed;
-		} else if(buttonHeld == true)
-		{
-			speed = startSpeed * -1;
 		}
 
 		fractJourney += speed * Time.deltaTime;
-		transform.position = Vector3.Lerp(startPos, player.transform.position, fractJourney);
+		transform.position = Vector3.Lerp(startPos, player.transform.position, fractJourney); //Moves the enemy fractJourney percent of the way toward/away from the player.
 
-		if (Vector2.Distance (new Vector2 (transform.position.x, transform.position.z), new Vector2 (player.transform.position.x, player.transform.position.z)) <= 1)
+		if (Vector2.Distance (new Vector2 (transform.position.x, transform.position.z), new Vector2 (player.transform.position.x, player.transform.position.z)) <= 1) //If the enemy is close enough to the player, run the damage function on the player.
 		{
-			print ("You took damage.");
 			player.SendMessage ("Damage");
 			Destroy (gameObject);
 		}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour 
 {
-	public int health;
+	public int health; //The player's current health.
 
-	void Damage()
+	void Damage() //Running this function subtracts one health from the player and prints a debug if health is <= 0.
 	{
 		health -= 1;
 		if (health <= 0)
