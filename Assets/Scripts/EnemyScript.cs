1e3c796b4835f53ff9e2129b0ca2b66ff89f2948
Clonch <tc553413@dhcp-237-148.cns.ohiou.edu>
2017-01-20 23:39:25 -0500

    Enemies can now be pushed back by holding their assigned button.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,26 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyMoveScript : MonoBehaviour 
+public class EnemyScript : MonoBehaviour 
 {
 	public GameObject player;
 	public Vector3 startPos;
 	public int health;
 	public bool buttonHeld;
-	public float speed;
+	public float startSpeed;
+	public char button;
 	float fractJourney = 0.01f;
+	float speed;
 
 	void Start ()
 	{
 		startPos = transform.position;
+		speed = startSpeed;
 	}
 	void Update () 
 	{
-		if(health > 0 && buttonHeld == false)
+		if (Input.GetButton(""+button) )
 		{
-			fractJourney += speed * Time.deltaTime;
-			transform.position = Vector3.Lerp(startPos, player.transform.position, fractJourney);
+			buttonHeld = true;
+			print ("Button pressed");
+		} else
+		{
+			buttonHeld = false;
 		}
+
+		if(buttonHeld == false)
+		{
+			speed = startSpeed;
+		} else if(buttonHeld == true)
+		{
+			speed = startSpeed * -1;
+		}
+
+		fractJourney += speed * Time.deltaTime;
+		transform.position = Vector3.Lerp(startPos, player.transform.position, fractJourney);
 
 		if (Vector2.Distance (new Vector2 (transform.position.x, transform.position.z), new Vector2 (player.transform.position.x, player.transform.position.z)) <= 1)
 		{
