d8742f6d2efaf36bc31a4b6218079005ab77290e
Clonch <tc553413@dhcp-237-148.cns.ohiou.edu>
2017-01-21 21:17:34 -0500

    Menu is implementd, but needs a quit button.

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour 
+{
+	void StartGame()
+	{
+		SceneManager.LoadScene ("Level01");
+	}
+	void QuitGame()
+	{
+		Application.Quit();
+	}
+	void Credits()
+	{
+		print ("Credits.");
+	}
+	void HighScore()
+	{
+		print ("High Score");
+	}
+}
