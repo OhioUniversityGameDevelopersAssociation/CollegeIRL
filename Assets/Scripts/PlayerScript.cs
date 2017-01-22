aad715eb5c49f8a2920b0f87a569b6301ac76a54
Clonch <tc553413@dhcp-237-148.cns.ohiou.edu>
2017-01-21 21:03:51 -0500

    Player now animates and changes animations as health decreases.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,6 +4,36 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour 
 {
 	public int health; //The player's current health.
+	Animator animator;
+	int state = 4;
+
+
+	void Start()
+	{
+		animator = GetComponent<Animator> ();
+	}
+
+
+	void Update()
+	{
+		if (health <= 0) 
+		{
+			print ("You're dead.");
+		} else if(health <= 2 && state == 2)
+		{
+			animator.SetTrigger ("NextState");
+			state--;
+		} else if(health <= 4 && state == 3)
+		{
+			animator.SetTrigger ("NextState");
+			state--;
+		} else if(health <= 6 && state == 4)
+		{
+			animator.SetTrigger ("NextState");
+			state--;
+		}
+	}
+
 
 	void Damage() //Running this function subtracts one health from the player and prints a debug if health is <= 0.
 	{
